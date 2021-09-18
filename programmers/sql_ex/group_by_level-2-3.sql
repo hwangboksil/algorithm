3a70f4787a998bc36e07981af50d5f0c8bbb6a22
황복실 <tlatms0908@gmail.com>
2021-09-19 01:16:15 +0900

    Algorithm: programmers group_by_level-2-3 REFACTOR 1. WHERE 구 -> HAVING 구 사용

--- programmers/sql_ex/group_by_level-2-3.sql
+++ programmers/sql_ex/group_by_level-2-3.sql
@@ -1,4 +1,9 @@
 -- 09:00부터 19:59까지, 각 시간대별로 입양이 몇 건이나 발생했는지 조회하는 SQL문을 작성해주세요. 이때 결과는 시간대 순으로 정렬해야 합니다.
-SELECT HOUR(datetime), COUNT(datetime) FROM animal_outs 
-WHERE 9 <= HOUR(datetime) AND HOUR(datetime) < 20
-GROUP BY HOUR(datetime) ORDER BY HOUR(datetime) ;
+-- HAVING 사용
+SELECT HOUR(datetime) AS h, COUNT(datetime) FROM animal_outs 
+GROUP BY h HAVING 9 <= h AND h < 20 ORDER BY h;
+
+-- WHERE 사용
+-- SELECT HOUR(datetime) AS h, COUNT(datetime) FROM animal_outs 
+-- WHERE 9 <= HOUR(datetime) AND HOUR(datetime) < 20
+-- GROUP BY h ORDER BY h;
