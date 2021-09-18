e38c4dd9facb34dc414330574812c3abe2afebd9
황복실 <tlatms0908@gmail.com>
2021-09-18 16:30:44 +0900

    Algorithm: programmers group_by_level-2-2

---
+++ programmers/sql_ex/group_by_level-2-2.sql
@@ -0,0 +1,2 @@
+-- 동물 이름 중 두 번 이상 쓰인 이름과 해당 이름이 쓰인 횟수를 조회하는 SQL문을 작성해주세요. 이때 결과는 이름이 없는 동물은 집계에서 제외하며, 결과는 이름 순으로 조회해주세요.
+SELECT name, COUNT(name) FROM animal_ins GROUP BY name HAVING COUNT(name) >= 2 ORDER BY name;
