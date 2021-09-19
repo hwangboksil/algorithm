1f2dc36340448fc2b589444dcf2072dbdce797a1
황복실 <tlatms0908@gmail.com>
2021-09-19 18:06:55 +0900

    Algorithm: nadoCoding python-basic_6-6_quiz-5 REFACTOR 1. while문 제거, 변수 수정, 코드 간결화

--- etc/yt-nadocoding/python-basic/6-6_quiz-5.py
+++ etc/yt-nadocoding/python-basic/6-6_quiz-5.py
@@ -15,18 +15,31 @@
 
 import sys, random
 
-while True :
-    user = 1 # 손님
-    myUser = 0 # 총 탑승 승객 수
-    for i in range(50) :
-        time = random.randrange(5, 51) # 소요 시간
-       # time = int(sys.stdin.readline())
-        if 5 <= (time) <= 15 :
-            print("[O] {0}번째 손님 (소요시간 : {1}분)".format(user, time))
-            user += 1
-            myUser += 1
-        else :
-            print("[ ] {0}번째 손님 (소요시간 : {1}분)".format(user, time))
-            user += 1
-    print("총 탑승 승객 : {0} 분".format(myUser))
-    break
+user = 0 # 총 탑승 승객 수
+
+for i in range(1, 51) :
+    time = random.randrange(5, 51) # 소요 시간
+   # time = int(sys.stdin.readline()) # 소요 시간 input
+    if 5 <= (time) <= 15 :
+        print("[O] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
+        user += 1
+    else :
+        print("[ ] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
+
+print("총 탑승 승객 : {0} 분".format(user))
+
+# while True :
+#     user = 1 # 손님
+#     myUser = 0 # 총 탑승 승객 수
+#     for i in range(50) :
+#         time = random.randrange(5, 51) # 소요 시간
+#        # time = int(sys.stdin.readline())
+#         if 5 <= (time) <= 15 :
+#             print("[O] {0}번째 손님 (소요시간 : {1}분)".format(user, time))
+#             user += 1
+#             myUser += 1
+#         else :
+#             print("[ ] {0}번째 손님 (소요시간 : {1}분)".format(user, time))
+#             user += 1
+#     print("총 탑승 승객 : {0} 분".format(myUser))
+#     break
