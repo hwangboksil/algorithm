6a9532fc50d7b86b30b6871a497a70260c0b428e
황복실 <tlatms0908@gmail.com>
2021-09-19 16:25:32 +0900

    Algorithm: baekjoon 10952번 A+B - 5 REFACTOR 1. 변수 제거하여 코드길이 줄임.

--- baekjoon/10952.py
+++ baekjoon/10952.py
@@ -1,11 +1,18 @@
 import sys
 
-A = 1
-B = 1
-
-while A != 0 and B != 0 :
+while True :
     A, B = map(int, sys.stdin.readline().split())
-    if A+B == 0 :
-        pass
+    if (A and B) == 0 :
+        break
     else :
         print(A+B)
+
+# A = 1
+# B = 1
+
+# while A != 0 and B != 0 :
+#     A, B = map(int, sys.stdin.readline().split())
+#     if A+B == 0 :
+#         pass
+#     else :
+#         print(A+B)
