b776a1ede66edbf19193e7d3b3ac85b6bb4fa436
황복실 <tlatms0908@gmail.com>
2021-09-24 13:43:15 +0900

    Algorithm: baekjoon 1712 손익분기점

--- baekjoon/Bronze-IV/1712.py
+++ baekjoon/Bronze-IV/1712.py
@@ -1,15 +1,21 @@
 A, B, C = map(int, input().split())
-n = 1 # 판매 수
 
-while True:
-    total = A + B * n # 생산 비용
-    purchase = C * n # 판매 굼액
+if B >= C:
+    print(-1)
+else:
+    print(A//(C-B)+1)
+
+# n = 1 # 판매 수
+
+# while True:
+#     total = A + B * n # 생산 비용
+#     purchase = C * n # 판매 금액
     
-    if B >= C: # 가변 비용은 판매 금액보다 크거나 같으면 이익이 발생하지 않는다.
-        print(-1)
-        break
-    elif total >= purchase: # 손익분기점 미만인 경우
-        n += 1 # 판매 수 증가
-    elif total < purchase: # 손익분기점 이상
-        print(n)
-        break
+#     if B >= C: # 가변 비용은 판매 금액보다 크거나 같으면 이익이 발생하지 않는다.
+#         print(-1)
+#         break
+#     elif total >= purchase: # 손익분기점 미만인 경우
+#         n += 1 # 판매 수 증가
+#     elif total < purchase: # 손익분기점 이상
+#         print(n)
+#         break
