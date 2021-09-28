fe0fc334fd34b5484f56f34cccd1d8541b327d05
황복실 <tlatms0908@gmail.com>
2021-09-28 10:51:06 +0900

    Algorithm: baekjoon 8393 n까지 연속한 수의 합 Refactor 1. 함수 사용 2. 가우스의 방법 적용

--- baekjoon/Bronze-V/8393.py
+++ baekjoon/Bronze-V/8393.py
@@ -1,8 +1,15 @@
-n = int(input())
-result = 0
-for i in range(n+1) :
-    result += i
-print(result)
+# 가우스 방법
+def sum_n(n):
+    return n * (n + 1) //2
+
+print(sum_n(int(input())))
+
+# 처음 작성한 코드
+# n = int(input())
+# result = 0
+# for i in range(n+1) :
+#     result += i
+# print(result)
 
 # 함수 사용
 # def sum_n(n):
