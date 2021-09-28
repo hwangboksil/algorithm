df91fd19c082ebfca839c9e081f1ecd37ad0e8c0
황복실 <tlatms0908@gmail.com>
2021-09-29 00:15:29 +0900

    Algorithm: programmers level-1 take_two_and_add Modify 1. itertools의 combination 함수인 조합 개념을 적용함 2. return 값에 sorted 함수를 사용하여 값을 고정함

--- programmers/python_ex/level-1/take_two_and_add.py
+++ programmers/python_ex/level-1/take_two_and_add.py
@@ -1,11 +1,27 @@
+from itertools import combinations
+
 def solution(numbers):
-   answer = []
-   n = 0
-   for i in numbers:
-       for j in range(len(numbers[:-2])):
-           answer.append(numbers[n] + numbers[j])
-       n += 1
-   return print(list(set(answer)))
+    answer = []
+    data = list(combinations(numbers, 2))
+    
+    for i in range(len(data)):
+        answer.append(sum(data[i]))
+
+    return sorted(list(set(answer)))
+
+# print(solution([2,1,3,4,1])) # 통과
+print(solution([5,0,2,7])) # 통과
+
+# -------------------------------------------
+
+# def solution(numbers):
+#    answer = []
+#    n = 0
+#    for i in numbers:
+#        for j in range(len(numbers[:-2])):
+#            answer.append(numbers[n] + numbers[j])
+#        n += 1
+#    return print(list(set(answer)))
 
 # 결과
 # [Input]
@@ -14,7 +30,7 @@ def solution(numbers):
 # [2, 3, 4, 5, 6, 7] 통과
 
 # [Input]
-solution([5,0,2,7])
+# solution([5,0,2,7])
 # [Output]
 # [0, 2, 5, 7, 10, 12] 탈락 
 
