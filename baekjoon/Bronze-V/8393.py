a9e55daa34935de136c0ecc5965ecadf66381d7f
황복실 <tlatms0908@gmail.com>
2021-09-28 10:42:40 +0900

    Algorithm: baekjoon 8393 n까지 연속한 수의 합

--- baekjoon/Bronze-V/8393.py
+++ baekjoon/Bronze-V/8393.py
@@ -3,3 +3,11 @@ result = 0
 for i in range(n+1) :
     result += i
 print(result)
+
+# def sum_n(n):
+#     s = 0
+#     for i in range(1, n+1):
+#         s += i
+#     return s
+
+# print(sum_n(int(input())))
