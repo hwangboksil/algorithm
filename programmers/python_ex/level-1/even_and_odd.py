4f985de8ff04640b2e006554839f054de4f3524f
황복실 <tlatms0908@gmail.com>
2021-09-29 00:21:47 +0900

    etc: 해당 코테 이름 작성

--- programmers/python_ex/level-1/even_and_odd.py
+++ programmers/python_ex/level-1/even_and_odd.py
@@ -1,3 +1,4 @@
+# 짝수와 홀수
 # 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수를 완성
 def solution(num):
     answer = ''
--- programmers/python_ex/level-1/take_two_and_add.py
+++ programmers/python_ex/level-1/take_two_and_add.py
@@ -1,3 +1,6 @@
+# 두 개 뽑아서 더하기
+# 정수 배열 numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return
+
 from itertools import combinations
 
 def solution(numbers):
