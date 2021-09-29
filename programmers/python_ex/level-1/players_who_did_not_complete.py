4f3e8be0bda37a567e171cf0fd329b9ca3e79185
황복실 <tlatms0908@gmail.com>
2021-09-30 00:10:22 +0900

    Algorithm: programmers players_who_did_not_complete Refactor 1. sort() 함수를 사용하여 두개의 리스트를 매치 후 매치되지 않은 이름을 answer에 삽입 2. break를 사용하여 코드 간략화

--- programmers/python_ex/level-1/players_who_did_not_complete.py
+++ programmers/python_ex/level-1/players_who_did_not_complete.py
@@ -1,13 +1,27 @@
 # 완주하지 못한 선수
 # 마라톤에 참여한 선수들의 이름 배열 participant, 완주한 선수들의 이름 배열 completion, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성
 
+# 효율성, 정확성 통과했지만 테스트 케이스 1/3 성공 이유는?
 def solution(participant, completion):
-   answer = ''
-   for i in range(0, len(participant)):
-       if participant[i] in completion :
-           completion.remove(participant[i])
-       else:
-           answer = participant[i] 
-   return answer
+    answer = ''
+    participant.sort()
+    completion.sort()
+    for i in range(len(participant)):
+        if participant[i] != completion[i] :
+            answer = participant[i]
+            break
+    return answer
 
 print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
+
+# #효율성 테스트 탈락 (시간초과)
+# def solution(participant, completion):
+#    answer = ''
+#    for i in range(0, len(participant)):
+#        if participant[i] in completion :
+#            completion.remove(participant[i])
+#        else:
+#            answer = participant[i] 
+#    return answer
+
+# print(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]))
