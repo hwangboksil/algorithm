8795697f1b24e4ee6fdb17cb3e986d1280fc225f
황복실 <tlatms0908@gmail.com>
2021-10-01 12:02:46 +0900

    Algorithm: programmers average Refactor 1. lambda와 reduce 함수 사용, 코드 간략화

--- programmers/python_ex/level-1/average.py
+++ programmers/python_ex/level-1/average.py
@@ -1,7 +1,13 @@
 # 평균 구하기
 # 정수를 담고 있는 배열 arr의 평균값을 return
 
+from functools import reduce
+
 def solution(arr):
-    answer = 0
-    answer = sum(arr) / len(arr)
+    answer = (reduce(lambda x, y: x + y, arr)) / len(arr)
     return answer
+
+# def solution(arr):
+#     answer = 0
+#     answer = sum(arr) / len(arr)
+#     return answer
