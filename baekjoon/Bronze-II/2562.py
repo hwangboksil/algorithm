d6e6a3b1d6621d8e9102710b169a9813888d2e81
황복실 <tlatms0908@gmail.com>
2021-10-03 16:23:44 +0900

    Algorithm: baekjoon 2562 최댓값

---
+++ baekjoon/Bronze-II/2562.py
@@ -0,0 +1,12 @@
+# 최댓값
+# 9개의 서로 다른 자연수가 주어질 때, 이들 중 최댓값을 찾고 그 최댓값이 몇 번째 수인지를 구하는 프로그램을 작성
+
+data = []
+
+for i in range(9):
+   data.append(int(input()))
+print(max(data), data.index(max(data))+1, sep='\n')
+
+# for문을 사용하여 코드 작성
+# - 9번의 loop를 돌면서 값을 한 줄씩 입력 받고, 빈 배열인 data에 값을 바로 넣는다.
+# - 최댓값을 구하는 함수 max와 최댓값이 몇 번째 수인지 파악하기 위해 index 메서드를 사용한다.
