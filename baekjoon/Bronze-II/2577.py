b34db98c1844d301099a916f60749b5510bfee34
황복실 <tlatms0908@gmail.com>
2021-10-04 19:58:45 +0900

    Algorithm: baekjoon 2577 숫자의 개수 Refactor 1. 불필요 함수 제거, 코드 간략화

--- baekjoon/Bronze-II/2577.py
+++ baekjoon/Bronze-II/2577.py
@@ -1,15 +1,26 @@
 # 숫자의 개수
 # 세 개의 자연수 A, B, C가 주어질 때 A × B × C를 계산한 결과에 0부터 9까지 각각의 숫자가 몇 번씩 쓰였는지를 구하는 프로그램을 작성
 
-result = []
 a,b,c = [int(input()) for _ in range(3)]
-nums = list(str((lambda a,b,c: a*b*c)(a,b,c)))
+nums = list(str(a*b*c))
 
 for i in range(10):
-    result.append(nums.count(str(i)))
-print(*result, sep='\n')
+    print(nums.count(str(i)))
+
+- a,b,c를 한 줄씩 입력 받을 수 있도록 list comprehension을 사용
+- 변수 nums에 a*b*c의 결과 값을 하나씩 저장할 수 있도록 str타입으로 변경 후 list로 저장한다.
+- for문에 range(10)을 주고 i 값을 int -> str로 변환 후 '0'인경우 nums에서 count를 사용하여 '0'의 개수를 세어 도출된 값을 출력하고 '9'까지 반복한다.
+
+# ===================================================
+# result = []
+# a,b,c = [int(input()) for _ in range(3)]
+# nums = list(str((lambda a,b,c: a*b*c)(a,b,c)))
+
+# for i in range(10):
+#     result.append(nums.count(str(i)))
+# print(*result, sep='\n')
 
 # - 빈 배열 result 선언, a,b,c를 한 줄씩 입력 받을 수 있도록 list comprehension을 사용
-# - 변수 nums에 lambda를 사용해 입력 받은 값들을 곱하여 리스트로 만든다.
+# - 변수 nums에 lambda를 사용해 입력 받은 값들을 곱하고, 결과 값을 한 개씩 나누기 위해 int -> str 타입으로 변경 후 리스트로 만든다.
 # - for문에 range(10)을 주고 i 값을 int -> str로 변환 후 '0'인경우 nums에서 count를 사용하여 '0'의 개수를 세어 도출된 값을 빈 배열인 result에 차례대로 넣어준다.
 # - 리스트 result를 언 패킹 하기위해 *를 붙여주고 도출된 값들이 한 줄씩 출력되어야 하므로 sep을 사용한다.
