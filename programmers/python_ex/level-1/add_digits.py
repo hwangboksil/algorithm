e089423d8a56e45270ee0910bf3bafb0efcc8792
hwangboksil <tlatms0908@gmail.com>
2021-10-13 11:20:08 +0900

    Algorithm: programmers 자릿수 더하기 Refacor 1. 입력값 list 담지않고 바로 사용

--- programmers/python_ex/level-1/add_digits.py
+++ programmers/python_ex/level-1/add_digits.py
@@ -3,9 +3,12 @@
 # 예를들어 N = 123이면 1 + 2 + 3 = 6을 return 
 
 def solution(n):
-    return sum([int(i) for i in list(str(n))])
+    # return sum([int(i) for i in list(str(n))])
+    return sum([int(i) for i in str(n)])
 
 print(solution(12345))
 
 # - 입력값을 하나씩 가져오기 위해 str타입으로 변환 후 list에 담아 for문을 돌리고 가져 온 값을 다시 int로 변환하여 list comprehension에 담아둔다.
 # - 반복문이 끝나면 sum() 함수를 사용해 리스트에 담긴 모든 값을 합하여 반환한다.
+
+#- 입력값을 반복문에 넣기 전 list에 담지 않아도 str 타입이면 하나씩 가져온다.
