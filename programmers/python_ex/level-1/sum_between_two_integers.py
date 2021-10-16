8e518e5b4fa484e82324a1338992ffc88bb3eded
hwangboksil <tlatms0908@gmail.com>
2021-10-16 22:11:28 +0900

    Algorithm: programmers 두 정수 사이의 합 Refactor 1. pass, list comprehension 제거

--- programmers/python_ex/level-1/sum_between_two_integers.py
+++ programmers/python_ex/level-1/sum_between_two_integers.py
@@ -3,13 +3,24 @@
 # 예를 들어 a = 5, b = 3인 경우, 3 + 4 + 5 = 12이므로 12를 리턴합니다.
 
 def solution(a, b):
-    if a > b:
-        a, b = b, a        
-        pass
-
-    return sum([int(i) for i in range(a, b+1)])
+    if a > b: a, b = b, a
+    
+    return sum(range(a, b+1))
 
 print(solution(5, 3))
+
+# 1. if문에서 Swap 후 바로 return으로 넘어가 pass는 필요없다.
+# 2. sum()함수는 iterable한 값은 모두 연산 가능하므로 range()만으로도 사용가능하다.
+
+# ===========================================================
+# def solution(a, b):
+#     if a > b:
+#         a, b = b, a        
+#         pass
+
+#     return sum([int(i) for i in range(a, b+1)])
+
+# print(solution(5, 3))
 
 # 1. 만약 a가 b보다 크면 a와 b를 Swap 후 return에서 a부터 b까지의 합을 반환한다.
 # 2. a가 b보다 작다면 return에서 a부터 b까지의 합을 반환한다.
