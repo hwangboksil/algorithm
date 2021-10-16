c3cb38d5e8f81c9a96ac1ee7c1ba3866835d73e7
hwangboksil <tlatms0908@gmail.com>
2021-10-16 22:41:52 +0900

    Algorithm: programmers 문자열 내 p와 y의 개수 Refactor 1. 조건문외의 Boolean 값은 제거

--- programmers/python_ex/level-1/numOfPAndYInString.py
+++ programmers/python_ex/level-1/numOfPAndYInString.py
@@ -3,12 +3,20 @@
 # 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
 
 def solution(s):
-    s = s.upper()
-    if s.count('P') != s.count('Y'): return False
-    
-    return True
+    return s.upper().count('P') == s.upper().count('Y')
 
 print(solution('pPoooyY'))
+
+# 1. 두 개의 값이 값으면 True, 다르면 False가 반환된다.
+
+# =====================================================
+# def solution(s):
+#     s = s.upper()
+#     if s.count('P') != s.count('Y'): return False
+    
+#     return True
+
+# print(solution('pPoooyY'))
 
 # 1. 입력받은 문자열 s를 전부 대문자로 변경한다.
 # 2. count()함수를 사용하여 문자열 내에 해당하는 알파벳을 세어 값이 틀리면 False를 반환한다.
