7d9abe4b31cc03572998461d007bce3d1aabf2f5
hwangboksil <tlatms0908@gmail.com>
2021-10-19 10:10:58 +0900

    Algorithm: programmers 정수 제곱근 판별 Refactor 1. math.sqrt() 제곱근 구하는 메서드 사용, 코드 간결화

--- programmers/python_ex/level-1/determineSquareRootOfInt.py
+++ programmers/python_ex/level-1/determineSquareRootOfInt.py
@@ -1,22 +1,36 @@
 # 정수 제곱근 판별
 # n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
 
+import math
+
 def solution(n):
-    answer = 0
-    n = n / 2
-    i = 1
-    while n-i >= 0:
-        n -= i
-        i += 1
-    answer = (i+1)**2
-    
-    if i != n*2:
-        answer = -1
-    
-    return answer
+    if n % math.sqrt(n) == 0:
+        return int((math.sqrt(n)+1)**2)
+    return -1
 
 print(solution(3))
 print(solution(121))
+
+# 1. math함수의 sqrt() 메서드를 사용하여 n의 제곱근을 구한 뒤 n과 나눈 나머지 값이 0과 같다면 (n의 제곱근+1)**2를 계산 후 float 타입 -> int 타입으로 변환 후 반환.
+# 2. n과 나눈 나머지 값이 0과 같지 않다면 -1을 반환
+
+# ====================================
+# def solution(n):
+#     answer = 0
+#     n = n / 2
+#     i = 1
+#     while n-i >= 0:
+#         n -= i
+#         i += 1
+#     answer = (i+1)**2
+    
+#     if i != n*2:
+#         answer = -1
+    
+#     return answer
+
+# print(solution(3))
+# print(solution(121))
 
 # 1. 제곱근 구하는 공식 
 #     121 / 2 = 60.5 -> 60.5 - 1 ~ 음수가되기 전까지의 정수
