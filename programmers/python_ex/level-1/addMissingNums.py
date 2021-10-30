94b482d88e1775786a7df9ee5ebb8db9280f15ba
hwangboksil <tlatms0908@gmail.com>
2021-10-30 23:11:37 +0900

    Algorithm: programmers 없는 숫자 더하기 Refactor 1. list comprehension 사용, 코드 간결화

--- programmers/python_ex/level-1/addMissingNums.py
+++ programmers/python_ex/level-1/addMissingNums.py
@@ -3,15 +3,24 @@
 # numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
 
 def solution(numbers):
-    answer = 0
-    
-    for i in range(10):
-        if i not in numbers:
-            answer += i
-    return answer
+    return sum([i for i in range(10) if i not in numbers])
 
 print(solution([1,2,3,4,6,7,8,0]))
 print(solution([5,8,4,0,6,7,9]))
+
+# 1. list comprehension을 사용하여 조건문에 맞는 i 값을 저장 후 sum 함수를 사용하여 결과 값의 합을 반환한다.
+
+# ======================================
+# def solution(numbers):
+#     answer = 0
+    
+#     for i in range(10):
+#         if i not in numbers:
+#             answer += i
+#     return answer
+
+# print(solution([1,2,3,4,6,7,8,0]))
+# print(solution([5,8,4,0,6,7,9]))
 
 # 1. range를 사용해 0부터 9까지 순차적으로 i에 저장한다.
 # 2. if문을 사용하여 배열 numbers에 i값이 들어있지 않다면 더해준다.
