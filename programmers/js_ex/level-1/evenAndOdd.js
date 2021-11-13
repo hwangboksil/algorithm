3abaf8bd1fe795c66bb3219cab404bbf1f6dc7bd
hwangboksil <tlatms0908@gmail.com>
2021-11-13 18:46:44 +0900

    Algorithm: programmers 짝수와 홀수 Refactor 1. 삼항 연산자를 사용하여 코드 간결화

--- programmers/js_ex/level-1/evenAndOdd.js
+++ programmers/js_ex/level-1/evenAndOdd.js
@@ -2,15 +2,25 @@
 // 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수를 작성해주세요.
 
 function solution(num) {
-  if (num % 2 === 0) {
-    return "Even";
-  } else {
-    return "Odd";
-  }
+  return num % 2 ? "Even" : "Odd";
 }
 
 console.log(solution(3));
 console.log(solution(4));
+
+// 1. 삼항연산자를 사용하여 코드 간결화
+
+// =====================================
+// function solution(num) {
+//   if (num % 2 === 0) {
+//     return "Even";
+//   } else {
+//     return "Odd";
+//   }
+// }
+
+// console.log(solution(3));
+// console.log(solution(4));
 
 // 1. if 문을 사용하여 인자 값 num을 2로 나눴을 떄 나머지가 0이면 'Even' 리턴
 // 2. 0이 아니면 'Odd' 리턴을 한다.
